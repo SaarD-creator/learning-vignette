a8adecd9075ea9b7f89f14fc3f7062728ede349f
Saar-creator <dayerssaar@gmail.com>
2026-03-10 21:13:14 +0100

    Update learning_vignette.py

--- learning_vignette.py
+++ learning_vignette.py
@@ -4,7 +4,24 @@ st.title("Learning vignette")
 
 st.write("Welcome to the learning vignette of group 48. Please start by answering the next question:")
 
-vraag = st.text_input("Which percentage of employees in the health sector quit within their first year?")
+col1, col2 = st.columns([3,1])
 
-if vraag:
-    st.write(f"Leuk je te ontmoeten, !")
+with col1:
+    antwoord = st.text_input("Which percentage of employees in the health sector quit within their first year?")
+
+with col2:
+    st.write("%")
+
+if antwoord:
+    try:
+        waarde = float(antwoord.replace(",", "."))
+
+        if 30 <= waarde <= 31:
+            st.success("Correct! Well done. The actual percentage is around 30%.")
+        elif 20 <= waarde <= 40:
+            st.info("You're close! Your estimate is in the right range, but not quite correct.")
+        else:
+            st.error("Your answer is quite far from the actual percentage. Try thinking about it again.")
+
+    except:
+        st.warning("Please enter a valid number.")
